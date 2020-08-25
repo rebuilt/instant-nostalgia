1cc87a02fba801a62aec75a91006c088e56f195f
rebuilt <memoryman51@hotmail.com>
2020-08-25 22:37:51 +0200

    Create photos by uploading them

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,22 @@ class PhotosController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to photos_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def photo_params
+    params.require(:photo).permit(images: [])
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,3 @@
 class Photo < ApplicationRecord
+  has_many_attached :images
 end
