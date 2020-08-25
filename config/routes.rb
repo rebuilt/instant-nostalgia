154f7ca9a6ab5fc7001ad2a68602208b89a0664c
rebuilt <memoryman51@hotmail.com>
2020-08-25 22:37:21 +0200

    added resources definition for photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root to: 'photos#index'
   get 'photos/new', to: 'photos#new'
   scope '/:locale' do
-    get 'photos/index'
-    get 'photos/show'
-    get 'photos/new'
+    resources :photos
   end
 end
