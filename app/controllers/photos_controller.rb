64bd0f019e87c997a3b5359c79db19819ea8ed29
rebuilt <memoryman51@hotmail.com>
2020-08-27 21:14:10 +0200

    prevent n+1 queries by attaching images to @photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def index
-    # @photos = Photo.all.with_attached_image
-    @photos = Photo.all
+    @photos = Photo.all.with_attached_image
+    # @photos = Photo.all
   end
 
   def show; end
