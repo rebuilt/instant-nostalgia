d78e17bb5268cf17a926c9ce1c83bcf28981637f
rebuilt <memoryman51@hotmail.com>
2020-08-27 13:49:31 +0200

    don't overwrite existing photos, append to collection instead

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Don't overwrite files when assigning active storage records
+  config.active_storage.replace_on_assign_to_many = false
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
