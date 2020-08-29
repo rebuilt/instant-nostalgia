33c23b5357b3a7f1d6200c8e1f2951e3d7f646e4
rebuilt <memoryman51@hotmail.com>
2020-08-29 08:11:47 +0200

    Made convertDMSToDD method more readable.  Made methods private

--- app/models/gps.rb
+++ app/models/gps.rb
@@ -3,11 +3,23 @@ class Gps
     Coordinates.new(convertDMSToDD(coordinates.latitude), convertDMSToDD(coordinates.longitude))
   end
 
+  private
+
   def convertDMSToDD(coordinate)
     units = parse(coordinate)
-    decimal = units[:degrees] + units[:minutes] / 60 + units[:seconds] / 3600
+    decimal = calculate(units)
+    decimal = add_sign(decimal, units)
+    decimal
+  end
+
+  def add_sign(decimal, units)
+    # Add a negative sign if direction is south or west.  Do nothing otherwise
     decimal *= -1 if units[:direction] == 'S' || units[:direction] == 'W'
     decimal
+  end
+
+  def calculate(units)
+    units[:degrees] + units[:minutes] / 60 + units[:seconds] / 3600
   end
 
   def parse(coordinate)
