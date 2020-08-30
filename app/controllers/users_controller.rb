ae61abb9eef44e624f09d03bbc1a1788cbe01127
rebuilt <memoryman51@hotmail.com>
2020-08-30 23:17:34 +0200

    added new and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to ideas_path
+    else
+      render 'new'
+    end
   end
+
+  def edit; end
 end
