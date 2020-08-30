13120e5462a57174c5cc18044a1a37a622f1a49b
rebuilt <memoryman51@hotmail.com>
2020-08-30 15:24:30 +0200

    If no location data present in EXIF, set latitude and longitude to 0 in decimal and degrees

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,16 +8,33 @@ class Photo < ApplicationRecord
   end
 
   def populate_with(metadata)
-    update(file_name: image.filename,
-           latitude_in_degrees: "#{metadata['GPSLatitude']}, #{metadata['GPSLatitudeRef']}",
-           longitude_in_degrees: "#{metadata['GPSLongitude']}, #{metadata['GPSLongitudeRef']}",
-           date_time_digitized: DateTime.strptime(metadata['DateTime'], '%Y:%m:%d %H:%M:%S'))
+    if metadata['GPSLatitude'].nil?
+      update(file_name: image.filename,
+             latitude_in_degrees: '0',
+             longitude_in_degrees: '0',
+             date_time_digitized: DateTime.strptime(metadata['DateTime'], '%Y:%m:%d %H:%M:%S'),
+             latitude_in_decimal: 0,
+             longitude_in_decimal: 0)
+    else
+      update(file_name: image.filename,
+             latitude_in_degrees: "#{metadata['GPSLatitude']}, #{metadata['GPSLatitudeRef']}",
+             longitude_in_degrees: "#{metadata['GPSLongitude']}, #{metadata['GPSLongitudeRef']}",
+             date_time_digitized: DateTime.strptime(metadata['DateTime'], '%Y:%m:%d %H:%M:%S'))
+    end
   end
 
   def initialize_latlong_decimals
+    return unless location?
+
     coordinates = Coordinates.new(latitude_in_degrees, longitude_in_degrees)
     coordinates = Gps.new.to_decimal(coordinates)
     update(latitude_in_decimal: coordinates.latitude,
            longitude_in_decimal: coordinates.longitude)
+  end
+
+  def location?
+    return false if latitude_in_degrees == '0' && longitude_in_degrees == '0'
+
+    true
   end
 end
