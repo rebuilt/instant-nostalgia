8c9941de6003703a9e0691c02099adce9b3bc2d7
rebuilt <memoryman51@hotmail.com>
2020-08-30 23:28:27 +0200

    implemented create action.  users can create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def show; end
 
@@ -11,11 +13,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to ideas_path
+      redirect_to photos_path
     else
       render 'new'
     end
   end
 
   def edit; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :password)
+  end
 end
