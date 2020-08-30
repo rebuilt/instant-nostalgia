8800868315c2488c820371160852e095ed17ab01
rebuilt <memoryman51@hotmail.com>
2020-08-30 11:57:17 +0200

    added coordinate to test

--- test/models/gps_test.rb
+++ test/models/gps_test.rb
@@ -4,16 +4,25 @@ class PhotoTest < ActiveSupport::TestCase
   test 'gps converts a degree/minute/second coordinate to a decimal degree coordinate' do
     gps = Gps.new
     lats = [
-      ' 46/1, 25/1, 151212/10000, N '
+      ' 46/1, 25/1, 151212/10000, N ',
+      '46/1, 26/1, 141960/10000, N ',
+      '51/1, 30/1, 22188/10000, N '
     ]
     longs = [
-      ' 6/1, 55/1, 262117/10000, E '
+      ' 6/1, 55/1, 262117/10000, E ',
+      '6/1, 55/1, 298926/10000, E ',
+      ' 0/1, 10/1, 395402/10000, W '
+
     ]
     expected_lats = [
-      46.420866999999994
+      46.420866999999994,
+      46.43727666666666,
+      51.50061633333333
     ]
     expected_longs = [
-      6.923947694444445
+      6.923947694444445,
+      6.924970166666667,
+      -0.17765005555555555
     ]
     lats.each_with_index do |lat, i|
       conversion(lat, longs[i], expected_lats[i], expected_longs[i])
