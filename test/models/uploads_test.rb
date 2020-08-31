85d54e9745724fe568d99f56e3ac9f15bd3f86cf
rebuilt <memoryman51@hotmail.com>
2020-08-31 20:27:15 +0200

    Add a test for file uploads

--- test/models/uploads_test.rb
+++ test/models/uploads_test.rb
@@ -16,6 +16,7 @@ class UploadsTest < ActiveSupport::TestCase
     assert_equal 0, photo.longitude_in_decimal
     assert_equal '0', photo.latitude_in_degrees
     assert_equal '0', photo.longitude_in_degrees
+    assert_equal '20170827_093118.jpg', photo.file_name
 
     count = ActiveStorage::Attachment.count
     image = fixture_file_upload(Rails.root.join('public', 'images', '20190731_125829.jpg'), 'image/jpg')
@@ -30,5 +31,6 @@ class UploadsTest < ActiveSupport::TestCase
     assert_in_delta 6.95111, photo.longitude_in_decimal, 0.01
     assert_equal '46/1, 25/1, 67229/10000, N', photo.latitude_in_degrees
     assert_equal '6/1, 57/1, 40169/10000, E', photo.longitude_in_degrees
+    assert_equal '20190731_125829.jpg', photo.file_name
   end
 end
