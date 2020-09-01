f25e62d9f3c7a5e6b422df681f758f3935ef560e
rebuilt <memoryman51@hotmail.com>
2020-09-01 18:56:26 +0200

    sort edit action in file to correct position

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit; end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,8 +20,6 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
-  def edit; end
 
   private
 
