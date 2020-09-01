d20af4201fad058aaea9b3f33ae6fe886ba70f95
rebuilt <memoryman51@hotmail.com>
2020-09-01 22:40:27 +0200

    added test

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can add photo to multiple albums' do
+    user = User.new(email: 'me@mail.com', password: '123', username: 'me')
+    photo = Photo.new(user: user)
+    album = Album.new(title: 'first', user: user)
+    album.photos << photo
+    assert album.save
+    second_album = Album.new(title: 'another', user: user)
+    second_album.photos << photo
+    assert second_album.save
+  end
 end
