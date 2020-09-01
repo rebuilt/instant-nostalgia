08626be8442a481dec6848f7446fe442a08cabba
rebuilt <memoryman51@hotmail.com>
2020-09-01 16:07:09 +0200

    added create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
-  def index
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to photos_path
+    else
+      flash[:alert] = 'Email or password were invalid.  Please try again'
+      render 'new'
+    end
   end
 end
