06ba16cc884256c4fa37d721137417dfa371b8f9
rebuilt <memoryman51@hotmail.com>
2020-09-01 17:36:06 +0200

    add logged_in and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
 end
