3a3e6c9bb7c47917705afa1a2d28a60e2096f5b4
rebuilt <memoryman51@hotmail.com>
2020-09-02 22:31:42 +0200

    added tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -7,4 +7,24 @@ class AlbumTest < ActiveSupport::TestCase
     user.albums << album
     user.save
   end
+
+  test 'can save a photo to an album' do
+    user = User.new(email: 'me@mail.com', username: 'me', password: '123')
+    album = Album.new(title: 'first')
+    user.albums << album
+    photo = Photo.new
+    album.photos << photo
+    album.save
+  end
+  test 'can only add a photo once to an album' do
+    user = User.new(email: 'me@mail.com', username: 'me', password: '123')
+    album = Album.new(title: 'first')
+    user.albums << album
+    photo = Photo.new(file_name: 'abc.jpg', user: user)
+    album.photos << photo
+    album.validate
+    puts album.errors.full_messages
+    puts photo.errors.full_messages
+    assert album.save
+  end
 end
