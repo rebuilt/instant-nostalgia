c2399472ba86829149276569c3b36848db844f58
rebuilt <memoryman51@hotmail.com>
2020-09-02 21:50:14 +0200

    added test

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user can save album' do
+    user = User.new(email: 'me@mail.com', username: 'me', password: '123')
+    album = Album.new(title: 'first')
+    user.albums << album
+    user.save
+  end
 end
