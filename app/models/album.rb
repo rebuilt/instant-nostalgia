d75133ac19f5af76ae6936535c34ceb823d03eca
rebuilt <memoryman51@hotmail.com>
2020-09-03 15:17:42 +0200

    set up many to many relationship to shared users

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :shares, class_name: 'User'
   has_and_belongs_to_many :photos
   validates :title, presence: true
 end
