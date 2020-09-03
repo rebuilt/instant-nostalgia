a9d68ac74960e3bcbd42b727551beb7c226915f3
rebuilt <memoryman51@hotmail.com>
2020-09-03 20:24:51 +0200

    populate index with albums shared with the user and by the user

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,7 +1,12 @@
 class SharesController < ApplicationController
   def index
+    @albums = Share.where(album_id: params[:id])
+    @shares = Share.where(user_id: current_user)
   end
 
   def new
+    puts params
   end
+
+  def create; end
 end
