68bde2b9a66da813243a7a64923bdd9c681a1192
rebuilt <memoryman51@hotmail.com>
2020-09-04 16:08:03 +0200

    added dependent destroy statement

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :user
-  has_many :shares
+  has_many :shares, dependent: :destroy
   has_many :users, through: :shares
   has_and_belongs_to_many :photos
   validates :title, presence: true
