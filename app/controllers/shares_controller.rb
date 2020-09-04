0d34197b679d6533fa8f357c5faad8105ef566e4
rebuilt <memoryman51@hotmail.com>
2020-09-04 18:45:39 +0200

    create a new share.  Don't include current user in list of users to share with

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -6,16 +6,28 @@ class SharesController < ApplicationController
 
   def new
     @album = Album.find(params[:album_id])
+    @share = Share.new
 
     if params[:search].present?
       @term = params[:search]
       @users = User.where('email LIKE ?', "%#{@term}%")
+      @users = @users.reject { |user| user == current_user }
     end
+
     respond_to do |format|
       format.html {}
       format.js { render :new }
     end
   end
 
-  def create; end
+  def create
+    user_id = params[:user_id]
+    album_id = params[:album_id]
+    @share = Share.new(user_id: user_id.to_i, album_id: album_id.to_i)
+    if @share.save
+      redirect_to shares_path
+    else
+      render :new
+    end
+  end
 end
