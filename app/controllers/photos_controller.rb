3c72f9baf7ae55726bf384934940ecfab293ac51
rebuilt <memoryman51@hotmail.com>
2020-09-05 21:35:31 +0200

    use with_attached_image method

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,11 +5,11 @@ class PhotosController < ApplicationController
     # TODO: Add photo modals
     # TODO: Add listener to recenter map on clicked-on photo
     # TODO: The current view should be extracted out into map#index and a new page for photos#index should be created here
-    @photos = Photo.all.include_image.belonging_to_user(current_user)
+    @photos = Photo.all.with_attached_image.belonging_to_user(current_user)
   end
 
   def show
-    @photo = Photo.include_image.belonging_to_user(current_user).find(params[:id])
+    @photo = Photo.with_attached_image.includes(:comments).belonging_to_user(current_user).find(params[:id])
     @comment = Comment.new
   end
 
