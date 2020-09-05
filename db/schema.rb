312ae212fad3844b98e73e3c42341231934efe1b
rebuilt <memoryman51@hotmail.com>
2020-09-05 09:55:33 +0200

    generated a comment model and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_073913) do
+ActiveRecord::Schema.define(version: 2020_09_05_075440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2020_09_05_073913) do
     t.bigint "album_id", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "photos", force: :cascade do |t|
     t.string "file_name"
     t.string "address"
@@ -106,6 +115,8 @@ ActiveRecord::Schema.define(version: 2020_09_05_073913) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "albums", "users"
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
   add_foreign_key "photos", "users"
   add_foreign_key "shares", "albums"
   add_foreign_key "shares", "users"
