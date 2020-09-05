720797b3f55d8dda3c169fc5d67862824b95f1d3
rebuilt <memoryman51@hotmail.com>
2020-09-05 08:23:27 +0200

    add todos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,10 @@
 class PhotosController < ApplicationController
   def index
+    # TODO: Add filter bar
+    # TODO: Add scrollbar
+    # TODO: Add photo modals
+    # TODO: Add listener to recenter map on clicked-on photo
+    # TODO: The current view should be extracted out into map#index and a new page for photos#index should be created here
     @photos = Photo.all.with_image.belongs_to_user(current_user)
   end
 
