92c6a7ff49d27803dd3ed062b8b8cd08cdaae094
rebuilt <memoryman51@hotmail.com>
2020-09-05 13:13:27 +0200

    added routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'login', to: 'session#new'
   get 'signup', to: 'user#new'
   scope '/:locale' do
-    resources :photos
+    resources :photos do
+      resources :comments
+    end
     resources :users
     resources :sessions, only: %i[new create destroy]
     resources :albums
