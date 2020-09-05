6364644669d9f25bb48aa2d92a3c75c8ab5c83e3
rebuilt <memoryman51@hotmail.com>
2020-09-05 08:21:35 +0200

    Add todos

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,6 +5,8 @@ class AlbumsController < ApplicationController
   end
 
   def show
+    # TODO: Non-owners should not be able to delete photos
+    # TODO: pagify album contents
     @album = Album.find(params[:id])
   end
 
