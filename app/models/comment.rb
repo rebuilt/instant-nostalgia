ac9fad54242c363e200aaf25522dc7ee9cd42086
rebuilt <memoryman51@hotmail.com>
2020-09-08 22:44:17 +0200

    order by date

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_rich_text :body
+
+  scope :order_by_old_to_new, -> { order('created_at DESC') }
 end
