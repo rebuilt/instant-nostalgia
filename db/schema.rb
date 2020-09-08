3c9cb15964345a3e3c8ffc448efe2972729aa2ab
rebuilt <memoryman51@hotmail.com>
2020-09-08 21:06:25 +0200

    added fields needed for polymorphic comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_075440) do
+ActiveRecord::Schema.define(version: 2020_09_08_171149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2020_09_05_075440) do
     t.bigint "photo_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "commentable_type"
+    t.integer "commentable_id"
     t.index ["photo_id"], name: "index_comments_on_photo_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
