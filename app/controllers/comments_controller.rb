f547f0a76766ccc0c37f033d70c61bff4dd12610
rebuilt <memoryman51@hotmail.com>
2020-09-08 21:20:36 +0200

    added create and destroy comments.  and corresponding create view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,38 @@
 class CommentsController < ApplicationController
+  before_action :load_commentable
+
   def create
-    photo = Photo.find(params[:photo_id])
-    @comment = Comment.new(comment_params)
+    @comment = @commentable.comments.new(allowed_params)
+    @comment.commentable = @commentable
     @comment.user = current_user
-    @comment.photo = photo
-    @comment.save
-    redirect_to photo_path(photo)
+    @comment.photo = Photo.find(5)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to @commentable }
+        format.js { render :create }
+      end
+    else
+      render :new
+    end
   end
 
-  def comment_params
-    params.require(:comment).permit(:body)
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @commentable }
+      format.js { render :destroy }
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:comment).permit(:body, :user_id, :commentable_id)
+  end
+
+  def load_commentable
+    resource, id = request.path.split('/')[2, 3]
+    @commentable = resource.singularize.classify.constantize.find(id)
   end
 end
