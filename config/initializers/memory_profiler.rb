1aa7ba11181c46af372bff3e1ace8bfa93ad7f0b
rebuilt <memoryman51@hotmail.com>
2020-09-11 23:22:31 +0200

    don't print to file since 'rails notes' command also picks up profile file

--- config/initializers/memory_profiler.rb
+++ config/initializers/memory_profiler.rb
@@ -1,5 +1,6 @@
 MemoryProfiler.start
 at_exit do
   report = MemoryProfiler.stop
-  report.pretty_print
+  report.pretty_print(color_output: true, scale_bytes: true)
+  # report.pretty_print(to_file: 'profile.txt', color_output: true, scale_bytes: true)
 end
