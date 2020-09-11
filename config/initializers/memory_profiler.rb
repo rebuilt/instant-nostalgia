b070b588ef9fefee8edf560206b32f34b7c49822
rebuilt <memoryman51@hotmail.com>
2020-09-11 23:34:42 +0200

    memory profiler crashed test environment.  Added guard clause to only profile in development

--- config/initializers/memory_profiler.rb
+++ config/initializers/memory_profiler.rb
@@ -1,6 +1,7 @@
+return unless ENV['RAILS_ENV'] == 'development'
+
 MemoryProfiler.start
 at_exit do
   report = MemoryProfiler.stop
   report.pretty_print(color_output: true, scale_bytes: true)
-  # report.pretty_print(to_file: 'profile.txt', color_output: true, scale_bytes: true)
 end
