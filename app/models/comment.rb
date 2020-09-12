996cd834f025086d4750da1c12e11a79995a3a6e
rebuilt <memoryman51@hotmail.com>
2020-09-12 19:45:08 +0200

    create input limit for text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_rich_text :body
+  # rich text surrounds body string in a div that adds 17 characters.  1017 allows us to set a front-end limit less than or equal to 1000 characters. Limiting rich text so that no one can insert a book as a comment.
+  validates :body, length: { maximum: 1017 }
 
   scope :order_by_old_to_new, -> { order('created_at DESC') }
 end
