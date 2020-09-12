caa35ce4c6d6a8ebc3dcb21c6819d2c321f31454
rebuilt <memoryman51@hotmail.com>
2020-09-12 19:44:11 +0200

    added create_user and create_photo methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,15 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def create_user(username = 'me', password = '123')
+    email ||= "#{username}@mail.com"
+    User.create(email: email, password: password, username: username)
+  end
+
+  def create_photo_with_attachment(user)
+    image = fixture_file_upload(Rails.root.join('test', 'images', '20190731_125829.jpg'), 'image/jpg')
+    photo = Photo.create(image: image, user: user)
+    photo.init
+    photo
+  end
 end
