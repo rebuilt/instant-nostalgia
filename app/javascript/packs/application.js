2d0fb3e49255f51b26d612590a57629b2811b2ef
rebuilt <memoryman51@hotmail.com>
2020-09-15 00:15:48 +0200

    webpacker broke.  I could not add javascripts through the javascript_pack_tag.  I had to perform a yarnclean.  Also: rm -rf node_modules; rails assets:clobber; yarn --check-files; yarn add @rails/activestorage; yarn add @rails/ujs; yarn add @rails/webpacker. Also yarn update @rails/webpacker because it did not install a working copy.  Also, I had to remove assets entry from .yarnclean.  Now I can add javascript files again.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,8 @@ require('channels')
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'controllers'
 
 require('trix')
 require('@rails/actiontext')
+
+import 'controllers'
