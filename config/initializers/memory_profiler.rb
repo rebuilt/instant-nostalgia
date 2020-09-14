8802ca6c0bc6292955cd324a145964454ef79dc7
rebuilt <memoryman51@hotmail.com>
2020-09-14 13:45:30 +0200

    turn off profiling

--- config/initializers/memory_profiler.rb
+++ config/initializers/memory_profiler.rb
@@ -1,7 +1,7 @@
-return unless ENV['RAILS_ENV'] == 'development'
+# return unless ENV['RAILS_ENV'] == 'development'
 
-MemoryProfiler.start
-at_exit do
-  report = MemoryProfiler.stop
-  report.pretty_print(color_output: true, scale_bytes: true)
-end
+# MemoryProfiler.start
+# at_exit do
+#   report = MemoryProfiler.stop
+#   report.pretty_print(color_output: true, scale_bytes: true)
+# end
