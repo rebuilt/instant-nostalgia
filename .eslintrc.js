9b112ceda950e8243c8aa9fd7d6fb12de38bbd26
rebuilt <memoryman51@hotmail.com>
2020-09-15 15:26:06 +0200

    reinitialized eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,36 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true,
-        node: true,
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
     },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        indent: ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
 }
