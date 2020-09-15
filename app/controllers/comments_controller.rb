d02f29c63796c1b3cda33770c9f2977f335a2f1b
rebuilt <memoryman51@hotmail.com>
2020-09-15 16:02:18 +0200

    add a count string

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,9 @@ class CommentsController < ApplicationController
     @comment = @commentable.comments.new(allowed_params)
     @comment.commentable = @commentable
     @comment.user = current_user
+
     if @comment.save
+      @count = count(@commentable.comments.count)
       respond_to do |format|
         format.html { redirect_to @commentable }
         format.js { render :create }
@@ -19,9 +21,9 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
+    @count = count(@commentable.comments.count)
     respond_to do |format|
       format.html { redirect_to @commentable }
-      # TODO: render :destroy
       format.js { render :destroy }
     end
   end
@@ -35,5 +37,9 @@ class CommentsController < ApplicationController
   def load_commentable
     resource, id = request.path.split('/')[2, 3]
     @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def count(num)
+    "#{num} #{'comment'.pluralize(num)}"
   end
 end
