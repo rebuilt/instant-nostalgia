6db85e1cfa3bfd25ba8d578ca28483cd551a02b1
rebuilt <memoryman51@hotmail.com>
2020-09-15 21:58:10 +0200

    respond to js format

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,11 @@
 class PhotosController < ApplicationController
   def index
-    # OPTIMIZE: Do these two lines do the same thing?  Am I correctly handling the n+1 problem?
     @photos = Photo.all.with_attached_image.belonging_to_user(current_user)
-    # @photos = Photo.all.includes(image_attachment: :blob).belonging_to_user(current_user)
   end
 
   def show
     @photo = Photo.with_attached_image.belonging_to_user(current_user).find(params[:id])
     @comments = Comment.with_rich_text_body.order_by_old_to_new.where(commentable_id: @photo).includes(:user)
-    # @comments = Comment.joins(:user).with_rich_text_body.order_by_old_to_new.where(commentable_id: @photo)
-    # @comments = Comment.with_rich_text_body.order_by_old_to_new.where(commentable_id: @photo).includes(:user)
-    # @comments = Comment.with_rich_text_body.order_by_old_to_new.joins(:user).where(commentable_id: @photo)
-    # @comments = Comment.with_rich_text_body.order_by_old_to_new.includes(:user).where(commentable_id: @photo)
     @comment = Comment.new
   end
 
@@ -37,7 +31,10 @@ class PhotosController < ApplicationController
   def destroy
     @photo = Photo.find(params[:id])
     @photo.destroy
-    redirect_to photos_path
+    respond_to do |format|
+      format.html { redirect_to photos_path }
+      format.js { render :destroy }
+    end
   end
 
   private
