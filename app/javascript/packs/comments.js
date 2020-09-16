39547c1e119d38197ade4453b7e2ea518a218bc9
rebuilt <memoryman51@hotmail.com>
2020-09-16 21:47:18 +0200

    move Comments to its own js file and attach it to the window object

---
+++ app/javascript/packs/comments.js
@@ -0,0 +1,88 @@
+const Comments = {}
+
+Comments.buildComment = function(comment){
+    const commentElement = document.createElement('li')
+    commentElement.setAttribute('class', 'comment')
+    commentElement.setAttribute('id', comment.id)
+
+    const comment__details = document.createElement('div')
+    comment__details.setAttribute('class', 'comment__details')
+
+    const avatar = document.createElement('img')
+    avatar.setAttribute('alt', 'Avatar')
+    avatar.setAttribute('width', '42')
+    avatar.setAttribute('src', comment.avatar_url)
+
+    const time = document.createElement('time')
+    time.setAttribute('datetime', comment.created_at)
+    time.setAttribute('class', 'comment__time')
+    time.textContent = comment.time_ago
+
+    const comment__body = document.createElement('p')
+    comment__body.setAttribute('class', 'comment__body')
+    comment__body.textContent = comment.body
+
+    const comment__name = document.createElement('span')
+    comment__name.setAttribute('class', 'comment__name')
+    comment__name.textContent = comment.first_name
+
+    const delete_form = document.createElement('form')
+    delete_form.setAttribute('class', 'button_to comment__delete' )
+    delete_form.setAttribute('method', 'post')
+    delete_form.setAttribute('action', comment.comment_url)
+    delete_form.setAttribute('data-remote', 'true')
+
+    const hidden1 = document.createElement('input')
+    hidden1.setAttribute('type', 'hidden')
+    hidden1.setAttribute('name', '_method')
+    hidden1.setAttribute('value', 'delete')
+
+    const delete_button = document.createElement('input')
+    delete_button.setAttribute('value', 'Delete')
+    delete_button.setAttribute('type', 'Submit')
+
+    const hidden2 = document.createElement('input')
+    hidden2.setAttribute('type', 'hidden')
+    hidden2.setAttribute('name','authenticity_token')
+    hidden2.setAttribute('value', comment.authenticity_token)
+
+    delete_form.appendChild(hidden1)
+    delete_form.appendChild(delete_button)
+    delete_form.appendChild(hidden2)
+
+    comment__details.appendChild(avatar)
+    comment__details.appendChild(comment__name)
+    comment__details.appendChild(time)
+    comment__details.appendChild(delete_form)
+
+    commentElement.appendChild(comment__details)
+    commentElement.appendChild(comment__body)
+    return commentElement
+}
+
+Comments.addComment = function(comment){
+    const newComment = Comments.buildComment(comment)
+
+    const commentList = document.getElementById('commentList')
+    commentList.appendChild(newComment)
+}
+
+Comments.clearInput = function(id){
+    const input = document.getElementById(id)
+    input.value = ''
+}
+
+Comments.updateCommentCount = function(comment, id){
+    const countElement1 = document.getElementById(id)
+    countElement1.textContent = comment.count
+}
+
+Comments.sayHello = function(){
+    console.log('Hello from comments js file')
+}
+
+Comments.removeComment = function(comment){
+    document.getElementById(comment.id).remove()
+}
+
+window.Comments = Comments
