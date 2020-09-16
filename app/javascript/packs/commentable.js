d2670b2b5401115745101199626f9d9348056a8a
rebuilt <memoryman51@hotmail.com>
2020-09-16 21:48:34 +0200

    rename commentable to comments

--- app/javascript/packs/commentable.js
+++
@@ -1,9 +0,0 @@
-console.log('inside the commentable object')
-
-const Commentable = { }
-
-Commentable.removeElement = function(commentable_id){
-    document.getElementById('id-'+commentable_id).parentElement.remove()
-}
-
-export default Commentable
