efa8f27f1cd669ac15d4e8416a5a30db919f8909
rebuilt <memoryman51@hotmail.com>
2020-09-16 13:18:18 +0200

    refactoring

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -10,18 +10,10 @@ class SharesController < ApplicationController
 
     if params[:search].present?
       @term = params[:search]
-      # TODO: pagify user search
+
       @users = User.search(@term)
-
-      # don't include self in list of users to share with
-      @users = @users.reject { |user| user == current_user }
-
-      # don't include users that already have access to the album
-      @album.users.each do |already_authorized_user|
-        @users = @users.reject do |user|
-          user == already_authorized_user
-        end
-      end
+      @users = remove_current_user(@users)
+      @users = remove_already_authorized_users(@album, @users)
     end
 
     respond_to do |format|
@@ -39,5 +31,20 @@ class SharesController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  def remove_current_user(users)
+    # don't include self in list of users to share with
+    users.reject { |user| user == current_user }
+  end
+
+  def remove_already_authorized_users(album, users)
+    # don't include users that already have access to the album
+    album.users.each do |already_authorized_user|
+      users = users.reject { |user| user == already_authorized_user }
+    end
+    users
   end
 end
