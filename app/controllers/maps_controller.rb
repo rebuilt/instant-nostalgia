c6887a1affbaa637d8dffb39de3eb873a8b7ef70
rebuilt <memoryman51@hotmail.com>
2020-09-19 23:22:22 +0200

    load suburb, village, city, state, country info

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,11 +1,19 @@
 class MapsController < ApplicationController
   def index
+    @photos = load_photos_by_area(:suburb)
+    @photos = load_photos_by_area(:village)
     @photos = load_photos_by_area(:city)
+    @photos = load_photos_by_area(:state)
+    @photos = load_photos_by_area(:country)
     # TODO: this should return most recent photos, not all photos
     @photos = Photo.all.with_attached_image.belonging_to_user(current_user) if @photos.nil?
     @photos = @photos.reject { |photo| photo.location? == false }
-    @cities = Photo.distinct.pluck(:city)
-    @cities = @cities.reject(&:nil?)
+
+    @suburbs = load_area_names(:suburb)
+    @villages = load_area_names(:village)
+    @cities = load_area_names(:city)
+    @states = load_area_names(:state)
+    @countries = load_area_names(:country)
   end
 
   private
@@ -19,5 +27,10 @@ class MapsController < ApplicationController
     end
 
     @photos
+  end
+
+  def load_area_names(area)
+    locations = Photo.distinct.pluck(area)
+    locations.reject(&:nil?)
   end
 end
