557daf66c6d57a9af388d01d084de2bbdbfd5214
rebuilt <memoryman51@hotmail.com>
2020-09-19 22:11:30 +0200

    don't return any nil values.  Extract out load_photos_by_area method. if no filters or photos found, just return all photos

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,16 +1,23 @@
 class MapsController < ApplicationController
   def index
-    if params[:checked]
-      params[:checked].each do |key, value|
-        next unless value == '1'
+    @photos = load_photos_by_area(:city)
+    # TODO: this should return most recent photos, not all photos
+    @photos = Photo.all.with_attached_image.belonging_to_user(current_user) if @photos.nil?
+    @photos = @photos.reject { |photo| photo.location? == false }
+    @cities = Photo.distinct.pluck(:city)
+    @cities = @cities.reject(&:nil?)
+  end
 
-        tmp = Photo.where(city: key)
-        @photos = @photos.present? ? @photos.or(tmp) : tmp
-      end
-    else
-      @photos = Photo.all.with_attached_image.belonging_to_user(current_user)
+  private
+
+  def load_photos_by_area(area)
+    params[area]&.each do |key, value|
+      next unless value == '1'
+
+      tmp = Photo.where(area => key)
+      @photos = @photos.present? ? @photos.or(tmp) : tmp
     end
 
-    @cities = Photo.distinct.pluck(:city)
+    @photos
   end
 end
