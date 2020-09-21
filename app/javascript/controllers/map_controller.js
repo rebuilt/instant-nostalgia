738639ccde429875502bb553bf4383f9249e22e6
rebuilt <memoryman51@hotmail.com>
2020-09-21 20:09:08 +0200

    created map_controller methods for interacting with map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,14 +9,69 @@
 
 import { Controller } from 'stimulus'
 
+let map
+let markers = []
 export default class extends Controller {
-  static targets = ['output', 'name'];
+    connect() {
+        this.element[this.identifier] = this
+    }
 
-  connect() {
-      this.outputTarget.textContent = 'Hello stimulus'
-  }
+    initMap(latitude, longitude) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: latitude, lng: longitude },
+            zoom: 12,
+        })
+    }
 
-  greet() {
-      this.outputTarget.textContent = `Hello ${this.nameTarget.value}`
-  }
+    addMarker(latitude, longitude, url, index) {
+        const image = {
+            url: url,
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32),
+        }
+
+        const shape = {
+            coords: [1, 1, 1, 20, 18, 20, 18, 1],
+            type: 'poly',
+        }
+
+        const marker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            icon: image,
+            shape: shape,
+            zIndex: index,
+        })
+        markers.push(marker)
+    }
+
+    center() {
+        const latitude = this.data.get('latitude')
+        const longitude = this.data.get('longitude')
+        let tmp
+
+        markers.forEach((item) => {
+            if (item.position.lat() == latitude && item.position.lng() == longitude) {
+                tmp = item
+            }
+        })
+        map.panTo(tmp.position)
+    }
+    // TODO: why can't I call this method here?
+    // center() {
+    //     const latitude = this.data.get('latitude')
+    //     const longitude = this.data.get('longitude')
+    //     map.panTo(this.getMarker().position)
+    // }
+
+    // getMarker(latitude, longitude) {
+    //     let tmp
+
+    //     markers.forEach((item) => {
+    //         if (item.position.lat() == latitude && item.position.lng() == longitude) {
+    //             tmp = item
+    //         }
+    //     })
+    //     return tmp
+    // }
 }
