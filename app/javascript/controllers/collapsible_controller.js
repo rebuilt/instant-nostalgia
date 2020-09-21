13fc9239b66019184cc6bd34eddc17882f9d4bdf
rebuilt <memoryman51@hotmail.com>
2020-09-21 14:06:18 +0200

    created stimulus controller for collapsible actions

---
+++ app/javascript/controllers/collapsible_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+    toggle(event) {
+        let button = event.target
+        button.classList.toggle('active')
+        let panel = button.nextElementSibling
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none'
+            let title = button.textContent.split(' ')
+            title[0] = 'Open'
+            button.textContent = `${title.join(' ')}`
+        } else {
+            panel.style.display = 'block'
+            let title = button.textContent.split(' ')
+            title[0] = 'Close'
+            button.textContent = `${title.join(' ')}`
+        }
+    }
+}
