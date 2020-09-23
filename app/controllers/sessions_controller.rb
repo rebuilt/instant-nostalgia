0a6c6e7421d293b1d0121b4a3b28d8f2e235da0a
rebuilt <memoryman51@hotmail.com>
2020-09-23 08:40:50 +0200

    redirect new sign ins to maps#index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to photos_path
+      redirect_to maps_path
     else
       flash[:alert] = 'Email or password were invalid.  Please try again'
       render 'new'
