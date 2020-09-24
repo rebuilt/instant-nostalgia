5d5ea5b1e19186aaaf9d72bad1d1f5722dd08d10
rebuilt <memoryman51@hotmail.com>
2020-09-24 19:53:18 +0200

    implemented load by date and load by date range

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,14 +2,17 @@ class MapsController < ApplicationController
   def index
     places = %i[city state country]
     places.each do |place|
-      @photos = load_photos_by_area(place) if params[place]
+      @photos = load_photos_by_area(place) if params[place].present?
     end
 
-    # @photos = load_date
+    @photos = load_by_date if params['day-selector'].present? && params['month-selector'].present?
 
-    @photos = load_albums if params[:album]
+    if params['start-date']['start-date(1i)'].present? && params['end-date']['end-date(1i)'].present?
+      @photos = load_by_date_range
+    end
 
-    # TODO: this should return most recent photos, not all photos
+    @photos = load_albums if params[:album].present?
+
     @photos = Photo.all.with_attached_image.belonging_to_user(current_user).most_recent if @photos.nil?
     @photos = remove_non_geocoded(@photos)
 
@@ -43,11 +46,35 @@ class MapsController < ApplicationController
     @photos
   end
 
-  def load_date
-    tmp = Photo.with_attached_image.belonging_to_user(current_user).where(date_time_digitized: '2019-06-21 13:54:44 UTC')
+  def load_by_date
+    return unless params['day-selector'].present? && params['month-selector'].present?
+
+    day = params['day-selector']
+    month = params['month-selector']
+    tmp = Photo.with_attached_image.belonging_to_user(current_user).day_is(day.to_i).month_is(month)
     @photos = @photos.present? ? @photos.or(tmp) : tmp
 
     @photos
+  end
+
+  def load_by_date_range
+    return unless params['start-date']['start-date(1i)'].present? && params['end-date']['end-date(1i)'].present?
+
+    start_date = parse_date(params['start-date'], 'start')
+
+    end_date = parse_date(params['end-date'], 'end')
+
+    tmp = Photo.with_attached_image.belonging_to_user(current_user).date_between(start_date, end_date)
+    @photos = @photos.present? ? @photos.or(tmp) : tmp
+
+    @photos
+  end
+
+  def parse_date(info, prefix)
+    year = info["#{prefix}-date(1i)"].to_i
+    month = info["#{prefix}-date(2i)"].to_i
+    day = info["#{prefix}-date(3i)"].to_i
+    Date.new year, month, day
   end
 
   def load_area_names(area)
