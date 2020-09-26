3d542802f86cca4ef6f3d6df71a77d00eb79813b
rebuilt <memoryman51@hotmail.com>
2020-09-26 15:49:23 +0200

    added createModal method

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,4 +4,38 @@ export default class extends Controller {
     initialize() {
         this.element[this.identifier] = this
     }
+
+    createModal(data) {
+        const modalContent = document.createElement('div')
+        modalContent.setAttribute('id', 'modal-content')
+        modalContent.setAttribute('class', 'modal-content')
+
+        const span = document.createElement('span')
+        span.setAttribute('class', 'close')
+        span.textContent = 'CLOSE X'
+
+        const image = document.createElement('img')
+        image.setAttribute('alt', `${data.latitude}, ${data.longitude}`)
+        image.setAttribute('width', '100%')
+        image.setAttribute('src', data.img_lg)
+
+        modalContent.appendChild(span)
+        modalContent.appendChild(image)
+
+        span.addEventListener('click', () => {
+            document.getElementById('modal-content').remove()
+            const modal = document.getElementById('modal')
+            modal.style.display = 'none'
+        })
+        return modalContent
+    }
+
+    show() {
+        const modal = document.getElementById('modal')
+        modal.style.display = 'block'
+    }
+    hide() {
+        const modal = document.getElementById('modal')
+        modal.style.display = 'none'
+    }
 }
