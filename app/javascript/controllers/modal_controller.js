46473f12f80f9a7069d068033e047fd74e63a609
rebuilt <memoryman51@hotmail.com>
2020-09-26 19:14:04 +0200

    added showModal method

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -23,13 +23,27 @@ export default class extends Controller {
         modalContent.appendChild(image)
 
         span.addEventListener('click', this.hide())
+        console.log('in creation method')
         return modalContent
     }
 
     show(content) {
         const modalDiv = document.getElementById('modal')
+        console.log(modalDiv)
         modalDiv.appendChild(content)
         modalDiv.style.display = 'block'
+    }
+
+    showModal() {
+        const id = this.data.get('id')
+        const url = this.data.get('url')
+        const data = {
+            id: id,
+            img_lg: url,
+        }
+        const content = this.createModal(data)
+        console.log(content)
+        this.show(content)
     }
 
     hide() {
