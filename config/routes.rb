605b97d473e9ec52a5cb725e30839b87db466538
rebuilt <memoryman51@hotmail.com>
2020-09-28 23:43:36 +0200

    added routes for toggle_public

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
     end
     resources :users
     resources :sessions, only: %i[new create destroy]
-    resources :albums
+    resources :albums, only: %i[index show create update destroy] do
+      post :toggle_public
+    end
     resources :photo_albums, only: %i[update destroy]
     resources :shares, only: %i[index new create]
     resources :maps, only: %i[index]
