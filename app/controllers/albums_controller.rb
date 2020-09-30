b6905b74e15cf2c97f4a578e01b9157ac3b2e229
rebuilt <memoryman51@hotmail.com>
2020-09-30 18:05:45 +0200

    remove variable from controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -11,8 +11,6 @@ class AlbumsController < ApplicationController
     # TODO: pagify album contents
 
     @album = Album.include_images.find(params[:id])
-    @public_status = 'Make this album private' if @album.public
-    @public_status = 'Make this album public' unless @album.public
   end
 
   def create
