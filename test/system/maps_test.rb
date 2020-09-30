6d4da6026548c88f5d3ac2995016cbe745b26d71
rebuilt <memoryman51@hotmail.com>
2020-09-30 15:03:43 +0200

    add tests

--- test/system/maps_test.rb
+++ test/system/maps_test.rb
@@ -1,9 +1,160 @@
 require 'application_system_test_case'
 
 class MapsTest < ApplicationSystemTestCase
-  test 'visiting the index' do
+  test 'registered user visiting the index' do
     sign_in(create_user)
     visit maps_path
     assert_current_path maps_path
+    assert page.has_content? 'Most recent'
+  end
+
+  test 'anonymous user visits index' do
+    user = create_user
+    create_album(user)
+    visit maps_path
+    assert page.has_content? 'Default public album'
+  end
+
+  test 'anonymous user can click on public album' do
+    user = create_user
+    album = create_album(user)
+    album.public = true
+    album.save
+
+    visit maps_path
+    click_on 'Open Public albums'
+    page.check("publicAlbum[#{album.id}]")
+    click_on 'Filter'
+    assert page.has_content? "Results for | publicAlbum: #{album.title}"
+  end
+
+  test 'anonymous user can click on multiple public albums' do
+    user = create_user
+    album = create_album(user)
+    album.public = true
+    album.save
+
+    user2 = create_user('bob')
+    album2 = create_album(user2)
+    album2.public = true
+    album2.save
+    visit maps_path
+    click_on 'Open Public albums'
+    page.check("publicAlbum[#{album.id}]")
+    page.check("publicAlbum[#{album2.id}]")
+    click_on 'Filter'
+    assert page.has_content? "Results for | publicAlbum: #{album.title} | publicAlbum: #{album2.title}"
+  end
+
+  test 'registered user can click on public album  ' do
+    user = create_user
+    sign_in(user)
+    album = create_album(user)
+    album.public = true
+    album.save
+
+    visit maps_path
+    click_on 'Open Public albums'
+    page.check("publicAlbum[#{album.id}]")
+    click_on 'Filter'
+    assert page.has_content? "Results for | publicAlbum: #{album.title}"
+  end
+
+  test 'registered user can click on multiple public albums  ' do
+    user = create_user
+    sign_in(user)
+    album = create_album(user)
+    album.public = true
+    album.save
+
+    user2 = create_user('bob')
+    album2 = create_album(user2)
+    album2.public = true
+    album2.save
+
+    visit maps_path
+    click_on 'Open Public albums'
+    page.check("publicAlbum[#{album.id}]")
+    page.check("publicAlbum[#{album2.id}]")
+    click_on 'Filter'
+    assert page.has_content? "Results for | publicAlbum: #{album.title}"
+  end
+
+  test 'most recent photos show up for registered user' do
+    user = create_user
+    sign_in(user)
+    photo0 = create_photo(0, user)
+    photo1 = create_photo(1, user)
+    visit maps_path
+    assert page.has_content? 'Most recent'
+    assert page.find("#photo-#{photo0.id}")
+    assert page.find("#photo-#{photo1.id}")
+  end
+
+  test 'can filter by date' do
+    user = create_user
+    sign_in(user)
+    photo0 = create_photo(0, user)
+    day = '10'
+    month = '1'
+
+    visit maps_path(params: { 'day-selector' => day, 'month-selector' => month })
+    assert page.has_content? "Results for | Date: #{day}-#{month}"
+    assert page.find("#photo-#{photo0.id}")
+  end
+
+  test 'can filter by city' do
+    user = create_user
+    sign_in(user)
+    photo0 = create_photo(0, user)
+    visit maps_path(params: { city: { photo0.city => '1' } })
+    assert page.has_content? "Results for | city: #{photo0.city}"
+  end
+
+  test 'can filter by state' do
+    user = create_user
+    sign_in(user)
+    photo0 = create_photo(0, user)
+    visit maps_path(params: { state: { photo0.state => '1' } })
+    assert page.has_content? "Results for | state: #{photo0.state}"
+  end
+
+  test 'can filter by country' do
+    user = create_user
+    sign_in(user)
+    photo0 = create_photo(0, user)
+    visit maps_path(params: { country: { photo0.country => '1' } })
+    assert page.has_content? "Results for | country: #{photo0.country}"
+  end
+
+  test 'can filter by album' do
+    user = create_user
+    sign_in(user)
+    album = create_album(user)
+    album.save
+
+    photo0 = create_photo(0, user)
+    album.photos << photo0
+    album.save
+
+    visit maps_path
+    click_on 'Open My albums'
+    page.check("album[#{album.id}]")
+    click_on 'Filter'
+    assert page.has_content? "Results for | album: #{album.title}"
+  end
+
+  test 'can filter by shared albums' do
+    user = create_user
+    user2 = create_user('you')
+    album = Album.new(title: 'first', user: user)
+    photo0 = create_photo(0, user)
+    album.photos << photo0
+    share = Share.new(user: user2, album: album)
+    share.save
+    sign_in(user2)
+
+    visit maps_path(params: { album: {album.id => '1'} })
+    assert page.has_content? "Results for | album: #{album.title}"
   end
 end
