0bf9a476eed7f050644151f2afd2820ecb13791a
rebuilt <memoryman51@hotmail.com>
2020-09-30 09:01:11 +0200

    create update and load_user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: %i[show edit update destroy]
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    # empty method
   end
 
   def new
     @user = User.new
-  end
-
-  def edit
-    @user = User.find(params[:id])
   end
 
   def create
@@ -26,9 +23,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # empty method
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :first_name, :last_name, :email, :password)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
   end
 end
