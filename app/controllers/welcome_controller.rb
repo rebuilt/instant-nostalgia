51f1e3bdd40da6c443e94df7e14dd5f60b5aa6b2
rebuilt <memoryman51@hotmail.com>
2020-09-30 18:06:14 +0200

    redirect authenticated users to photos page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_authenticated, only: %i[index]
   def index
+    # empty method
+  end
+
+  def redirect_authenticated
+    redirect_to photos_path if logged_in?
   end
 end
