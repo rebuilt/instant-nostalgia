ef09759f785af3166834fb70a0601d0bb9ece373
rebuilt <memoryman51@hotmail.com>
2020-10-02 10:51:36 +0200

    removed explicit @photos assignment.  Used DateTools methods

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,15 +1,17 @@
+require_relative 'date_tools'
+
 class MapsController < ApplicationController
   def index
     flash[:message] = 'Results for | '
     if logged_in?
-      @photos = load_photos_by_area(:city) if params[:city].present?
-      @photos = load_photos_by_area(:state) if params[:state].present?
-      @photos = load_photos_by_area(:country) if params[:country].present?
-      @photos = load_by_date if date_populated?
-      @photos = load_by_date_range if date_range_populated?
-      @photos = load_albums(:album) if params[:album].present?
-      @photos = load_albums(:publicAlbum) if params[:publicAlbum].present?
-      @photos = load_most_recent if @photos.nil?
+      load_photos_by_area(:city) if params[:city].present?
+      load_photos_by_area(:state) if params[:state].present?
+      load_photos_by_area(:country) if params[:country].present?
+      load_by_date if DateTools.date_populated?(params)
+      load_by_date_range if DateTools.date_range_populated?(params)
+      load_albums(:album) if params[:album].present?
+      load_albums(:publicAlbum) if params[:publicAlbum].present?
+      load_most_recent if @photos.nil?
       @photos = remove_non_geocoded(@photos)
       @cities = load_area_names(:city)
       @states = load_area_names(:state)
@@ -18,8 +20,8 @@ class MapsController < ApplicationController
       @shared_albums = current_user.authorized_albums
     else
       # If the user is not signed in
-      @photos = load_albums(:publicAlbum) if params[:publicAlbum].present?
-      @photos = load_default_album if @photos.nil?
+      load_albums(:publicAlbum) if params[:publicAlbum].present?
+      load_default_album if @photos.nil?
     end
 
     @public_albums = Album.where(public: true)
@@ -27,16 +29,20 @@ class MapsController < ApplicationController
 
   private
 
+  def load(photos)
+    @photos = @photos.present? ? @photos.or(photos) : photos
+  end
+
   def load_default_album
     album = User.first.albums.first
     add_message('Default public album', album.title)
-    album.photos
+    @photos = album.photos
   end
 
   def load_most_recent
     count = 5
     add_message('Most recent', count.to_s)
-    Photo.all.with_attached_image.belonging_to_user(current_user).most_recent_mappable(count)
+    @photos = Photo.all.with_attached_image.belonging_to_user(current_user).most_recent_mappable(count)
   end
 
   def add_message(filtered_by, value)
@@ -49,7 +55,8 @@ class MapsController < ApplicationController
 
       add_message(area, key)
       tmp = Photo.with_attached_image.belonging_to_user(current_user).where(area => key)
-      @photos = @photos.present? ? @photos.or(tmp) : tmp
+
+      load(tmp)
     end
 
     @photos
@@ -64,6 +71,7 @@ class MapsController < ApplicationController
       add_message(name, album.title)
       tmp = Photo.with_attached_image.joins(:albums).where(albums: { id: key })
 
+      # Can't use load method here because I have joined on :albums.  This mixes two activerecord model types. Meaning I will not be able to use .or method to join activerecord relations.  Instead I have to use the plus operator to join two arrays.  Any calls to load method after this point using .or will fail because @photos has been changed to an array from an activerecord relation.  Any operations on @photos after this method must assume it is an array, not a relation.  This is related to bug fixed on checkout 585fd28
       @photos = @photos.present? ? @photos + tmp : tmp
     end
     @photos
@@ -80,68 +88,27 @@ class MapsController < ApplicationController
     month = params['month-selector']
     add_message('Date', "#{day}-#{month}: (day-month)")
     tmp = Photo.with_attached_image.belonging_to_user(current_user).day_is(day).month_is(month)
-    @photos = @photos.present? ? @photos.or(tmp) : tmp
-    @photos
+
+    load(tmp)
   end
 
   def load_by_date_range
-    start_date = parse_date(params['start-date'], 'start')
+    start_date = DateTools.parse_date(params['start-date'], 'start')
 
-    end_date = parse_date(params['end-date'], 'end')
+    end_date = DateTools.parse_date(params['end-date'], 'end')
 
     add_message('Date range', "#{start_date} - #{end_date}")
     tmp = Photo.with_attached_image.belonging_to_user(current_user).date_between(start_date, end_date)
-    @photos = @photos.present? ? @photos.or(tmp) : tmp
-    @photos
-  end
 
-  def parse_date(info, prefix)
-    year = info["#{prefix}-date(1i)"].to_i
-    month = info["#{prefix}-date(2i)"].to_i
-    day = info["#{prefix}-date(3i)"].to_i
-    month = valid_month(prefix) if month == 0
-    day, month, year = valid_day(day, month, year, prefix) if day == 0
-    Date.new year, month, day
-  end
-
-  def valid_month(prefix)
-    'start' == prefix ? 1 : 12
-  end
-
-  def valid_day(day, month, year, prefix)
-    day = 1
-    month, year = add_one_to_month(month, year) if prefix == 'end'
-
-    [day, month, year]
-  end
-
-  def add_one_to_month(month, year)
-    if month < 12
-      month += 1
-    else
-      year += 1
-      month = 1
-    end
-    [month, year]
-  end
-
-  def load_area_names(area)
-    locations = Photo.belonging_to_user(current_user).distinct.pluck(area)
-    locations.reject(&:nil?)
+    load(tmp)
   end
 
   def remove_non_geocoded(photos)
     photos.reject { |photo| photo.location? == false }
   end
 
-  def date_range_populated?
-    params['start-date'].present? &&
-      params['start-date']['start-date(1i)'].present? &&
-      params['end-date'].present? &&
-      params['end-date']['end-date(1i)'].present?
-  end
-
-  def date_populated?
-    params['day-selector'].present? && params['month-selector'].present?
+  def load_area_names(area)
+    locations = Photo.belonging_to_user(current_user).distinct.pluck(area)
+    locations.reject(&:nil?)
   end
 end
