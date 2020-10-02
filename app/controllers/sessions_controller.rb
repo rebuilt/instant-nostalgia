9a646252c9d49c94e93e5af957fe9300c1bf20b7
rebuilt <memoryman51@hotmail.com>
2020-10-02 15:39:33 +0200

    correct user variable to @user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
+    @user = User.find_by(email: params[:email].downcase)
 
-    if user.present? && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to maps_path
     else
-      flash[:alert] = 'Email or password were invalid.  Please try again'
       render 'new'
     end
   end
