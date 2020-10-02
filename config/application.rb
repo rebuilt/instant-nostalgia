106a731de90579efa969ac13504954a9e56910ba
rebuilt <memoryman51@hotmail.com>
2020-10-02 15:37:07 +0200

    search for subfolders for translation files

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module CapstoneProjectWadC5S4750
     end
 
     config.i18n.default_locale = :en
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
