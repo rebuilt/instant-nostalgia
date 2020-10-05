efd92c6b047166d11119a774a5dea9ad4fc406f5
rebuilt <memoryman51@hotmail.com>
2020-10-05 10:23:19 +0200

    add translations

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module CapstoneProjectWadC5S4750
       g.test_framework :test_unit, fixture: false
     end
 
-    I18n.available_locales = %i[en es fr]
+    I18n.available_locales = %i[en es fr pt de it]
     config.i18n.default_locale = :en
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
