22192935099e8ed948555036a09a765978763d1d
rebuilt <memoryman51@hotmail.com>
2020-10-06 11:55:59 +0200

    redirect_authenticated set to private

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
   before_action :redirect_authenticated, only: %i[index]
+
   def index
     # empty method
   end
+
+  private
 
   def redirect_authenticated
     redirect_to photos_path if logged_in?
