c52af6a1f72eba1023e8f23687db101e7c81911f
rebuilt <memoryman51@hotmail.com>
2020-10-08 17:13:57 +0200

    populate address data as job

--- app/controllers/photo_uploads_controller.rb
+++ app/controllers/photo_uploads_controller.rb
@@ -2,18 +2,21 @@ class PhotoUploadsController < ApplicationController
   def create
     @photos = []
     params[:image].each do |blob|
+      @uploads_remaining = current_user.remaining_uploads
+      break unless @uploads_remaining.positive?
+
       @photo = Photo.new(image: blob, user: current_user)
       @photo.save
       @photo.init
       @photos << @photo
     end
 
-    @photos.each do |photo|
-      sleep(2.seconds)
-      photo.init_address
+    # ReverseGeocodeJob.perform_later @photos
+    @photos.each_with_index do |photo, index|
+      ReverseGeocodeJob.set(wait: index.seconds).perform_later(photo)
     end
 
-    redirect_to photos_path
+    redirect_to photos_path, notice: 'Addresses are being processed in the background.  Please allow at least 2 seconds per photo for processing.  Some photos may not have location information and will not be mappable.'
   end
 
   private
