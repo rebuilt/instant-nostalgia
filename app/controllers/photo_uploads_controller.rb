f69fc905bff0681ea886ca725d66f6a2fab1b0cb
rebuilt <memoryman51@hotmail.com>
2020-10-08 22:22:00 +0200

    use reverse_geocode_job to populate address data in the background

--- app/controllers/photo_uploads_controller.rb
+++ app/controllers/photo_uploads_controller.rb
@@ -1,22 +1,16 @@
 class PhotoUploadsController < ApplicationController
   def create
-    @photos = []
-    params[:image].each do |blob|
-      @uploads_remaining = current_user.remaining_uploads
-      break unless @uploads_remaining.positive?
+    @uploads_remaining = current_user.remaining_uploads
+    params[:image].each_with_index do |blob, index|
+      next unless @uploads_remaining.positive?
 
-      @photo = Photo.new(image: blob, user: current_user)
-      @photo.save
+      @photo = Photo.create(image: blob, user: current_user)
       @photo.init
-      @photos << @photo
-    end
 
-    # ReverseGeocodeJob.perform_later @photos
-    @photos.each_with_index do |photo, index|
-      ReverseGeocodeJob.set(wait: index.seconds).perform_later(photo)
-    end
+      @uploads_remaining -= 1
 
-    redirect_to photos_path, notice: 'Addresses are being processed in the background.  Please allow at least 2 seconds per photo for processing.  Some photos may not have location information and will not be mappable.'
+      ReverseGeocodeJob.set(wait: index.seconds).perform_later(@photo)
+    end
   end
 
   private
