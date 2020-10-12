b4791de163b8ad3f30cfc496d6665ae5f4b484ce
rebuilt <memoryman51@hotmail.com>
2020-10-12 18:33:23 +0200

    add ordering

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,6 @@ class Comment < ApplicationRecord
   # rich text surrounds body string in a div that adds 17 characters.  1017 allows us to set a front-end limit less than or equal to 1000 characters. Limiting rich text so that no one can insert a book as a comment.
   validates :body, length: { maximum: 1017 }
 
-  scope :order_by_old_to_new, -> { order('created_at DESC') }
+  scope :order_by_old_to_new, -> { order('created_at ASC') }
+  scope :order_by_new_to_old, -> { order('created_at DESC') }
 end
