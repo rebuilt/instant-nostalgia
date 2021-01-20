fc77c8ba95bed812bb9fb3374864aed4bada01e6
rebuilt <memoryman51@hotmail.com>
2021-01-20 13:11:23 +0100

    add methods to shares_helper

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -1,2 +1,14 @@
 module SharesHelper
+  def remove_current_user(users)
+    # don't include self in list of users to share with
+    users.reject { |user| user == current_user }
+  end
+
+  def remove_already_authorized_users(album, users)
+    # don't include users that already have access to the album
+    album.users.each do |already_authorized_user|
+      users = users.reject { |user| user == already_authorized_user }
+    end
+    users
+  end
 end
