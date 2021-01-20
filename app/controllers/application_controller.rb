34b392d18580b84386b1a5910dfa0ffd507bb404
rebuilt <memoryman51@hotmail.com>
2021-01-20 12:58:56 +0100

    Moved authorization methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
-  helper_method :logged_in?, :current_user, :ensure_logged_in, :is_owner?
+  helper_method :logged_in?, :current_user, :ensure_logged_in, :is_owner?, :can_view_photo?, :can_view_album?
   add_flash_types :success
 
   def set_locale
@@ -30,5 +30,28 @@ class ApplicationController < ActionController::Base
 
   def is_owner?(user, model)
     user == model.user
+  end
+
+  def can_view_photo?(photo)
+    # Can view the photo if it's public
+    photo.albums.each do |album|
+      return true if album.public
+    end
+
+    # Can view the photo if it's been shared with them
+    photo.authorized_users.each do |user|
+      return true if logged_in? && current_user == user
+    end
+
+    # Can view the photo if it belongs to them
+    return true if logged_in? && current_user == photo.user
+
+    false
+  end
+
+  def can_view_album?(album)
+    return true if album.public
+
+    logged_in? && (current_user == album.user || current_user.authorized_albums.include?(album))
   end
 end
