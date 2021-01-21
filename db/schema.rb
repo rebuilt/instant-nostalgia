043b16b271088d1eeb786149115bf7a27207764b
rebuilt <memoryman51@hotmail.com>
2021-01-21 19:00:16 +0100

    ran rails app:update to fix errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_145044) do
+ActiveRecord::Schema.define(version: 2021_01_21_174559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
