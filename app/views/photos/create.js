d2106d070ffbb3b2e3e23642e92d9cae4a8d566f
rebuilt <memoryman51@hotmail.com>
2021-01-25 16:42:48 +0100

    change view on upload page after a successful upload

---
+++ app/views/photos/create.js
@@ -0,0 +1,4 @@
+var resultPanel = document.getElementById('result')
+resultPanel.style.display = 'block'
+var uploadControls = document.getElementById('upload-controls')
+uploadControls.style.display = 'none'
